926f7c51e39ab0bcc1dacf794b459c1be9abdfba
MarthaN <m.allen@gmx.at>
2023-01-08 19:50:35 +0100

    Refactor views and templates - add test view and template

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def project_list(request):
-    return render(request, 'projects/index.html')
+from projects.models import Project
+
+def all_projects(request):
+    # query the db to return all project objects
+    projects = Project.objects.all()
+    return render(request,"projects/all_projects.html", {'projects': projects})
+
