a4d61250895eb4f4c72c188d5117b9c5a180bd43
MarthaN <m.allen@gmx.at>
2023-01-09 13:36:07 +0100

    Create superuser and add new project

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,6 +21,6 @@ from django.urls import include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('dddmin/', admin.site.urls),
     path("projects/", include('projects.urls'))
 ]
