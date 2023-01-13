c71bc435e76a58c06e090849e042f936268712a5
MarthaN <m.allen@gmx.at>
2023-01-13 20:53:45 +0100

    Add models and migrations -  category and posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Posts
+from .models import Category
+
+admin.site.register(Posts)
+admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
+import datetime
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+class Posts(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created = models.DateField(auto_now_add=True)
+    last_update = models.DateField(auto_now=True)
+    category = models.ManyToManyField('Category', related_name='posts')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "posts"
